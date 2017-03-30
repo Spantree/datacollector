d8df658d14b3be6f9af2884ea3cc13098a4648f6
Marija Spaic <marija@spantree.net>
2017-03-30 14:03:39 -0500

    Add handling for parent metadata when indexing in elasticsearch

--- elasticsearch-protolib/src/main/java/com/streamsets/pipeline/stage/destination/elasticsearch/ElasticsearchTarget.java
+++ elasticsearch-protolib/src/main/java/com/streamsets/pipeline/stage/destination/elasticsearch/ElasticsearchTarget.java
@@ -173,9 +173,9 @@ public class ElasticsearchTarget extends BaseTarget {
       validateEL(
               typeEval,
               conf.parentIdTemplate,
-              "elasticSearchConfig.docIdTemplate",
-              Errors.ELASTICSEARCH_04,
-              Errors.ELASTICSEARCH_05,
+              "elasticSearchConfig.parentIdTemplate",
+              Errors.ELASTICSEARCH_27,
+              Errors.ELASTICSEARCH_28,
               issues
       );
     }
@@ -241,12 +241,12 @@ public class ElasticsearchTarget extends BaseTarget {
         String index = getRecordIndex(elVars, record);
         String type = typeEval.eval(elVars, conf.typeTemplate, String.class);
         String id = null;
-        String parentId = null;
         if (!StringUtils.isEmpty(conf.docIdTemplate)) {
           id = docIdEval.eval(elVars, conf.docIdTemplate, String.class);
         }
+        String parent = null;
         if (!StringUtils.isEmpty(conf.parentIdTemplate)) {
-          parentId = docIdEval.eval(elVars, conf.parentIdTemplate, String.class);
+          parent = parentIdEval.eval(elVars, conf.parentIdTemplate, String.class);
         }
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         DataGenerator generator = generatorFactory.getGenerator(baos);
@@ -280,7 +280,7 @@ public class ElasticsearchTarget extends BaseTarget {
           // No header attribute set. Use default.
           opCode = conf.defaultOperation.code;
         }
-        bulkRequest.append(getOperation(index, type, id, recordJson, opCode));
+        bulkRequest.append(getOperation(index, type, id, parent, recordJson, opCode));
       } catch (IOException ex) {
         errorRecordHandler.onError(
             new OnRecordErrorException(
@@ -348,33 +348,44 @@ public class ElasticsearchTarget extends BaseTarget {
     return batchTime;
   }
 
-  private String getOperation(String index, String type, String id, String record, int opCode) {
+  private String getOperation(String index, String type, String id, String parent, String record, int opCode) {
     StringBuilder op = new StringBuilder();
     switch (opCode) {
       case OperationType.UPSERT_CODE:
-        if (StringUtils.isEmpty(id)) {
-          op.append(String.format("{\"index\":{\"_index\":\"%s\",\"_type\":\"%s\"}}%n", index, type));
-        } else {
-          op.append(String.format("{\"index\":{\"_index\":\"%s\",\"_type\":\"%s\",\"_id\":\"%s\"}}%n", index, type, id));
-        }
+        op.append(String.format("{\"index\":%s}%n", getOperationMetadata(index, type, id, parent)));
         op.append(String.format("%s%n", record));
         break;
       case OperationType.INSERT_CODE:
-        op.append(String.format("{\"create\":{\"_index\":\"%s\",\"_type\":\"%s\",\"_id\":\"%s\"}}%n", index, type, id));
+        op.append(String.format("{\"create\":%s}%n", getOperationMetadata(index, type, id, parent)));
         op.append(String.format("%s%n", record));
         break;
       case OperationType.UPDATE_CODE:
-        op.append(String.format("{\"update\":{\"_index\":\"%s\",\"_type\":\"%s\",\"_id\":\"%s\"}}%n", index, type, id));
+        op.append(String.format("{\"update\":%s}%n", getOperationMetadata(index, type, id, parent)));
         op.append(String.format("{\"doc\":%s}%n", record));
         break;
       case OperationType.DELETE_CODE:
-        op.append(String.format("{\"delete\":{\"_index\":\"%s\",\"_type\":\"%s\",\"_id\":\"%s\"}}%n", index, type, id));
+        op.append(String.format("{\"delete\":%s}%n", getOperationMetadata(index, type, id, parent)));
         break;
       default:
         LOG.error("Operation {} not supported", opCode);
         throw new UnsupportedOperationException(String.format("Unsupported Operation: %s", opCode));
     }
     return op.toString();
+  }
+
+  private String getOperationMetadata(String index, String type, String id, String parent) {
+    StringBuilder indexOp = new StringBuilder();
+
+    indexOp.append(String.format("{\"_index\":\"%s\",\"_type\":\"%s\"", index, type));
+    if (!StringUtils.isEmpty(id)) {
+      indexOp.append(String.format(",\"_id\":\"%s\"", id));
+    }
+    if (!StringUtils.isEmpty(parent)) {
+      indexOp.append(String.format(",\"parent\":\"%s\"", parent));
+    }
+    indexOp.append("}");
+
+    return indexOp.toString();
   }
 
   private List<ErrorItem> extractErrorItems(JsonObject json) {
